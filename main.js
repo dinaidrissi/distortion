99721df7eaa30c84509da82cf6b93b25c3712956
Dina Idrissi <dinaidrissi@mikkel-pc.kea.local>
2018-05-29 13:29:37 +0200

    hej

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 "use strict";
 //fecth the json file
+document.addEventListener("DOMContent", getJson);
 
+async function getJson(){
+    let dataJson = await fetch("https://kea-alt-del.dk/kata-distortion/");
+    let myJson = await dataJson.json;
+    console.log(myJson);
+}
 //put into the template
+
+//refresh every 10 sec
 
 //animation
