fc60ba2b15b683cfbf0c2a71fa94648d051cc22b
Dina Idrissi <dinaidrissi@Dinas-MacBook-Pro.local>
2018-06-04 09:17:11 +0200

    still working on the show data

--- main.js
+++ main.js
@@ -4,11 +4,22 @@ document.addEventListener("DOMContentLoaded", getJson);
 
 async function getJson(){
     let dataJson = await fetch("https://kea-alt-del.dk/kata-distortion/");
-    let myJson = await dataJson.json;
+    let myJson = await dataJson.json();
     console.log(myJson);
+
 }
 //put into the template
+functin showData(data){
+    let list = document.querySelector("#info");
+    let myTemplate = document.querySelector("#infostemplate").content;
 
+    let clone = myTemplate.cloneNode(true);
+   clone.querySelector("#infoscontainer").textContent = list.inQueue;
+   console.log(list.inQueue);
+}
 //refresh every 10 sec
+setTimeout(function(){
+    window.location.reload(1);
+ }, 5000);
 
 //animation
