80e723fa5698d5abf804fd337ea8ecce808dabd6
Dina Idrissi <dinaidrissi@Dinas-MacBook-Pro.local>
2018-06-04 09:40:29 +0200

    link to style

--- main.js
+++ main.js
@@ -25,28 +25,46 @@ showData(myJson);
         clone.querySelector("#loggedAt").textContent = data.loggedAt;
         list.appendChild(clone);
 }
-// //fecth the json file
-// document.addEventListener("DOMContentLoaded", getJson);
 
-// async function getJson(){
-//     let dataJson = await fetch("https://kea-alt-del.dk/kata-distortion/");
-//     let myJson = await dataJson.json();
-//     console.log(myJson);
-
-// }
-// //put into the template
-// function showData(data){
-//     let list = document.querySelector("#infostemplate");
-//     let myTemplate = document.querySelector("#info").content;
-
-//     let clone = list.cloneNode("true");
-//    //clone.querySelector(".queue").textContent = list.inQueue;
-//    console.log(clone.querySelector(".queue").textContent);
-//    myTemplate.appendChild(clone);
-// }
-// //refresh every 10 sec
-// setTimeout(function(){
-//     window.location.reload(1);
-//  }, 5000);
-
-// //animation
+/* set radius for all circles */
+var r = 400;
+var circles = document.querySelectorAll('.circle');
+var total_circles = circles.length;
+for (var i = 0; i < total_circles; i++) {
+    circles[i].setAttribute('r', r);
+}
+/* set meter's wrapper dimension */
+var meter_dimension = (r * 2) + 100;
+var wrapper = document.querySelector('#wrapper');
+wrapper.style.width = meter_dimension + 'px';
+wrapper.style.height = meter_dimension + 'px';
+/* add strokes to circles  */
+var cf = 2 * Math.PI * r;
+var semi_cf = cf / 2;
+var semi_cf_1by3 = semi_cf / 3;
+var semi_cf_2by3 = semi_cf_1by3 * 2;
+document.querySelector('#outline_curves')
+    .setAttribute('stroke-dasharray', semi_cf + ',' + cf);
+document.querySelector('#low')
+    .setAttribute('stroke-dasharray', semi_cf + ',' + cf);
+document.querySelector('#avg')
+    .setAttribute('stroke-dasharray', semi_cf_2by3 + ',' + cf);
+document.querySelector('#high')
+    .setAttribute('stroke-dasharray', semi_cf_1by3 + ',' + cf);
+document.querySelector('#outline_ends')
+    .setAttribute('stroke-dasharray', 2 + ',' + (semi_cf - 2));
+document.querySelector('#mask')
+    .setAttribute('stroke-dasharray', semi_cf + ',' + cf);
+/*bind range slider event*/
+var slider = document.querySelector('#slider');
+var lbl = document.querySelector("#lbl");
+var mask = document.querySelector('#mask');
+var meter_needle =  document.querySelector('#meter_needle');
+function range_change_event() {
+    var percent = slider.value;
+    var meter_value = semi_cf - ((percent * semi_cf) / 100);
+    mask.setAttribute('stroke-dasharray', meter_value + ',' + cf);
+    meter_needle.style.transform = 'rotate(' + (270 + ((percent * 180) / 100)) + 'deg)';
+    lbl.textContent = percent + '%';
+}
+slider.addEventListener('input', range_change_event);
